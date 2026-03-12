9b7bb2d7462022eaaf0bec8239d15ee78fe2ad30
tanyamishra26 <tanyamishra260@gmail.com>
2026-03-13 01:19:54 +0530

    changing model

--- rag/rag_chain.py
+++ rag/rag_chain.py
@@ -31,11 +31,11 @@ Question:
 )
 
     llm = HuggingFaceEndpoint(
-        repo_id="mistralai/Mistral-7B-Instruct-v0.2",
+        repo_id="google/flan-t5-large",
         huggingfacehub_api_token=st.secrets["HF_TOKEN"],
         temperature=0.2,
         max_new_tokens=512
-    )
+        )
 
     rag_chain = (
         RunnableParallel(
